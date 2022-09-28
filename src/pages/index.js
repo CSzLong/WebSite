770a93bcfac7813d1c48443172fd547e5ad016b6
孙老师-Chris <szlong1987@gmail.com>
2022-09-28 14:07:20 +0800

    new repository

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h1>Aptos World</h1>
+        <h3>Welcome to Aptos World Community</h3>
+        <p>亚太地区首个专注于资讯教育、招聘孵化、开发者社区的Aptos生态服务机构</p>
+      </div>
+    </section>
+  )
 }
